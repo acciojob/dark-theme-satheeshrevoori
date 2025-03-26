f8a44f66af76841c538e41c27c36cd6621708cf5
Satheesh Revoori <104457018+satheeshrevoori@users.noreply.github.com>
2025-03-26 18:44:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 function swapTheme() {
     let paraElement = document.getElementById("app");
     let buttonElement = document.getElementById("swap");
-    paraElement.classList.toggle('day');
     paraElement.classList.toggle('night');
-    buttonElement.classList.toggle('button_day');
     buttonElement.classList.toggle('button_night');
 }
