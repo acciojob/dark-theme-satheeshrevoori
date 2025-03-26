797721a5713f3cc8d0bcf3694b43d8c272ff1b2c
Satheesh Revoori <104457018+satheeshrevoori@users.noreply.github.com>
2025-03-26 18:00:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+ <script src="./script.js">
 //your code here
-
+function  swapTheme ()
+        {
+            let paraElement = document.getElementById("app");
+            let buttonElement =  document.getElementById("swap");
+            paraElement.style.backgroundColor = "black";
+            buttonElement.style.backgroundColor = " white";
+        
+        }
+	 </script>
