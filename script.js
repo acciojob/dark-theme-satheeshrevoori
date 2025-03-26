98d8f72a1dffdd14f6ccebfcadff201bac85a174
Satheesh Revoori <104457018+satheeshrevoori@users.noreply.github.com>
2025-03-26 18:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
- <script src="./script.js">
-//your code here
-function  swapTheme ()
-        {
-            let paraElement = document.getElementById("app");
-            let buttonElement =  document.getElementById("swap");
-            paraElement.style.backgroundColor = "black";
-            buttonElement.style.backgroundColor = "white";
-        
-        }
-	 </script>
+function swapTheme() {
+    let paraElement = document.getElementById("app");
+    let buttonElement = document.getElementById("swap");
+    paraElement.classList.toggle('day');
+    paraElement.classList.toggle('night');
+    buttonElement.classList.toggle('button_day');
+    buttonElement.classList.toggle('button_night');
+}
